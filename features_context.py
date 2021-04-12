ad85ce7ae1a251ca6a50f27b5e813f90c58ea04f
jessievs <jessievanschijndel@gmail.com>
2021-04-12 21:06:56 +0200

    Context embedding features AlBERT computed.

--- features_context.py
+++ features_context.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from tqdm import tqdm
 import torch
-from transformers import AlbertTokenizer, AlbertModel, DistilBertTokenizer, DistilBertModel
+from transformers import pipeline
+
 
 print('Loading collection...')
 
@@ -17,44 +18,30 @@ print('Creating contextual models...')
 albert = "albert-large-v2"
 distilbert = "distilbert-base-uncased"
 
-# We need to create the model and tokenizer
-t_albert = AlbertTokenizer.from_pretrained(albert)
-m_albert = AlbertModel.from_pretrained(albert)
-t_distilbert = DistilBertTokenizer.from_pretrained(distilbert)
-m_distilbert = DistilBertModel.from_pretrained(distilbert)
+# We need to create the pipeline
+qa_albert = pipeline('question-answering', model=albert, tokenizer=albert)
+qa_distilbert = pipeline('question-answering', model=distilbert, tokenizer=distilbert)
 
-def compute_features(data, queries):
+def compute_features(data, queries, data_size):
     # Creating new features for NLP features
     NLP_data = []
 
     # Going through all query-document pairs
-    for index in tqdm(range(len(data))):
+    for index in tqdm(range(data_size)):
         row = data.loc[index]
 
         # Identifying relevant query and passage/document
         query = queries[queries['qid'] == row['qid']]['query'].values[0]
         passage = passages[passages['pid'] == row['docid']]['passage'].values[0]
 
-        # Computing model input
-        input_albert = t_albert.encode_plus(query, passage, return_tensors='pt')
-        input_distilbert = t_distilbert.encode_plus(query, passage, return_tensors='pt')
-
-        # Obtaining model output probabilities
-        output_albert = m_albert(**input_albert)
-        output_distilbert = m_distilbert(**input_distilbert)
-
-        last_hidden_state_albert = output_albert.last_hidden_state
-        last_hidden_state_distilbert = output_distilbert.last_hidden_state
-
-        # Compute the logits
-        softmax = torch.nn.Softmax(dim=1)
-        logits_albert = softmax(last_hidden_state_albert)
-        logits_distilbert = softmax(last_hidden_state_distilbert)
+        # Computing answer according to passage and its score
+        result_albert = qa_albert(context=passage, question=query).get('score')
+        result_distilbert = qa_distilbert(context=passage, question=query).get('score')
 
         # Adding NLP features to data
-        NLP_data.append([row['qid'], row['docid'], row['rating'], p_albert, p_distilbert])
+        NLP_data.append([row['qid'], row['docid'], row['rating'], result_albert, result_distilbert])
 
-    NLP_data = pd.DataFrame(NLP_data, columns=['qid', 'docid', 'rating', 'albert', 'distilbert'])
+    NLP_data = pd.DataFrame(NLP_data, columns=['qid', 'docid', 'rating', 'score_qa_albert', 'score_qa_distilbert'])
 
     return NLP_data
 
@@ -63,8 +50,8 @@ print('DONE')
 print('Loading training data...')
 
 # Load training data
-training_data = pd.read_csv('core/training_data.csv', index_col=0)
-training_data_nonrelevant = pd.read_csv('core/training_data_nonrelevant.csv', index_col=0)
+training_data = pd.read_csv('features_core/training_data.csv', index_col=0)
+training_data_nonrelevant = pd.read_csv('features_core/training_data_nonrelevant.csv', index_col=0)
 training_data = training_data.append(training_data_nonrelevant).reset_index()
 queries_train = pd.read_csv('collections/msmarco-passage/collectionandqueries/queries.train.tsv', sep='\t',
                             names=['qid', 'query'])
@@ -76,7 +63,7 @@ print('Calculating features...')
 
 # # Obtaining features of text
 
-training_data_NLP = compute_features(training_data, queries_train)
+training_data_NLP = compute_features(training_data, queries_train, 20000)
 del training_data, queries_train
 
 print('DONE')
@@ -84,7 +71,7 @@ print('DONE')
 print('Saving to CSV...')
 
 # Save to csv
-training_data_NLP.to_csv('training_data_NLP.csv')
+training_data_NLP.to_csv('features_context/training_data_NLP.csv')
 del training_data_NLP
 
 print('DONE')
@@ -92,8 +79,8 @@ print('DONE')
 print('Loading testing data...')
 
 # Load testing data
-testing_data = pd.read_csv('core/testing_data.csv', index_col=0)
-testing_data_nonrelevant = pd.read_csv('core/testing_data_nonrelevant.csv', index_col=0)
+testing_data = pd.read_csv('features_core/testing_data.csv', index_col=0)
+testing_data_nonrelevant = pd.read_csv('features_core/testing_data_nonrelevant.csv', index_col=0)
 testing_data = testing_data.append(testing_data_nonrelevant).reset_index()
 queries_test = pd.read_csv('collections/msmarco-passage/msmarco-test2019-queries.tsv', sep = '\t',
                            names=['qid', 'query'])
@@ -103,7 +90,7 @@ print('DONE')
 
 print('Calculating features...')
 
-testing_data_NLP = compute_features(testing_data, queries_test)
+testing_data_NLP = compute_features(testing_data, queries_test, len(testing_data))
 del testing_data, queries_test
 
 print('DONE')
@@ -111,7 +98,7 @@ print('DONE')
 print('Saving to CSV...')
 
 # Save to csv
-testing_data_NLP.to_csv('testing_data_NLP.csv')
+testing_data_NLP.to_csv('features_context/testing_data_NLP.csv')
 del testing_data_NLP  # free up space
 
 print('DONE')
@@ -119,8 +106,8 @@ print('DONE')
 print('Loading validation data....')
 
 # Load validation data
-validation_data = pd.read_csv('core/validation_data.csv', index_col=0)
-validation_data_nonrelevant = pd.read_csv('core/validation_data_nonrelevant.csv', index_col=0)
+validation_data = pd.read_csv('features_core/validation_data.csv', index_col=0)
+validation_data_nonrelevant = pd.read_csv('features_core/validation_data_nonrelevant.csv', index_col=0)
 validation_data = validation_data.append(validation_data_nonrelevant).reset_index()
 queries_val = pd.read_csv('collections/msmarco-passage/collectionandqueries/queries.dev.small.tsv', sep = '\t', \
                           names=['qid', 'query'])
@@ -130,7 +117,7 @@ print('DONE')
 
 print('Calculating features...')
 
-validation_data_NLP = compute_features(validation_data, queries_val)
+validation_data_NLP = compute_features(validation_data, queries_val, len(validation_data))
 del validation_data, queries_val
 
 print('DONE')
@@ -138,7 +125,7 @@ print('DONE')
 print('Saving to CSV...')
 
 # Save to csv
-validation_data_NLP.to_csv('validation_data_NLP.csv')
+validation_data_NLP.to_csv('features_context/validation_data_NLP.csv')
 del validation_data_NLP
 
 print('DONE')
